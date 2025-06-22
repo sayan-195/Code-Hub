9dbf4a8d839b619e6dac38ff7b2546a621c4cea2
sayan-195 <sayanghosh0195@gmail.com>
2025-06-22 08:10:00 +0530

    Add new file

---
+++ palindromic_String.java
@@ -0,0 +1,29 @@
+public class palindromic_String {
+
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                if (isPalindrome(str.substring(i, j)) == true) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
